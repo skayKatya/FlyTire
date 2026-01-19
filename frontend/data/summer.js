149c5a5c81b2f1489fe4567cdc46919516b67d08
Kate <skay.katya@gmail.com>
2026-01-19 21:09:43 +0200

    landing page2

---
+++ frontend/data/summer.js
@@ -0,0 +1,12 @@
+export const summerTires = [
+  {
+    name: "Michelin Pilot Sport 4",
+    price: "$180",
+    size: "225/45R17",
+    season: "summer",
+    image: "./images/michelin.jpg",
+    amount: 5
+  }
+];
+
+
